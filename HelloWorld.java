85854b61ff66bb0ac766d716faa6a52a7886802e
Jwestbellarmine <jwest4@bellarmine.edu>
2024-08-27 14:21:53 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scnr = new Scanner (System.in);
+		System.out.println("Welcome back to Class! What is your name?");
+		String name = scnr.next();
+		System.out.println("Hello, " + name + "!");
+		
+
+	}
+
+}
+ 
